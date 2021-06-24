34dbce1a9b47f6248a4d02bbea18c29318a41aaf
tuhin-thinks <tuhinmitra190221@gmail.com>
2021-06-24 22:56:21 +0530

    new Js learnings

---
+++ first_app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello this is my first app...</h1>
+      <br></br>
+      <h1>With React.Js</h1>
+    </div>
+  );
+}
+
+export default App;
